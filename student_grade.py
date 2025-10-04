eb54d1a13b434e3d5a92990d7c3f7be3b9e745d5
deeksha <deekshavernekar11@gmail.com>
2025-10-04 09:16:04 +0530

    student grade evoluation program

---
+++ student_grade.py
@@ -0,0 +1,16 @@
+# student_grade.py
+
+marks = int(input("Enter marks: "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 40:
+    grade = "D"
+else:
+    grade = "Fail"
+
+print("Grade:", grade)
